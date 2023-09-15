bf414e0bebf51716e083576e85e60478690428c6
Frengky Gunawan <frengky.gunawan@erajaya.com>
2023-09-15 23:05:46 +0700

    feat(home): implement UI from mockup

---
+++ src/data/charities.js
@@ -0,0 +1,14 @@
+export const charities = [
+	{
+		id: 1,
+		title: 'First Charity Project',
+		category: 'Money',
+		thumbnail: 'https://th.bing.com/th/id/OIP.avb9nDfw3kq7NOoP0grM4wHaEK?pid=ImgDet&rs=1',
+		pledged: 1000,
+		target: 100000,
+		end_date: +new Date('10 Oct 2023'),
+		profile_photo: 'https://th.bing.com/th/id/OIP.rmim2jYzNpSCslo60INohQHaF9?pid=ImgDet&rs=1',
+		profile_name: 'Budi Doe',
+		no_pledges: 2
+	}
+];
---
+++ src/pages/Home.svelte
@@ -0,0 +1,15 @@
+
+<script>
+  import CharityList from "../components/CharityList.svelte";
+	import Footer from "../components/Footer.svelte";
+	import Header from "../components/Header.svelte";
+	import Promo from "../components/Promo.svelte";
+	import Welcome from "../components/Welcome.svelte";
+  import { charities } from "../data/charities.js";
+</script>
+
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
